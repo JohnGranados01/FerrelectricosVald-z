f7ac91316963c503b236e3d2f208e786f3bc8f41
JohnGranados01 <jhongranadossalamanca@gmail.com>
2021-09-17 23:06:07 -0500

    modificado para conexion con BD

--- database.php
+++ database.php
@@ -3,7 +3,7 @@
 $server = 'localhost:3308';
 $username = 'root';
 $password = '';
-$database = 'FerrelectricosValdez';
+$database = 'ferrelectricosvaldez';
 
 try {
   $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
